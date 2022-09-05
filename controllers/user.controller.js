999805f0bff27274351f5c8891c416e1457fb8cd
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2022-09-06 00:06:37 +0600

    vaildate body input

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,6 +16,8 @@ module.exports.getRandomUser = (req, res) => {
 
 // get all user
 module.exports.getAllUser = (req, res) => {
+  const limit = Number(req.query.limit);
+  console.log(limit);
   fs.readFile('user.json', (err, data) => {
     if (err) {
       res.write('Failed to read file ');
@@ -23,69 +25,89 @@ module.exports.getAllUser = (req, res) => {
     } else {
       const userString = data.toString();
       const users = JSON.parse(userString);
-      res.json(users);
+      if (limit && limit <= users.length) {
+        res.json(users.slice(0, limit));
+      } else {
+        res.send(users);
+      }
     }
   });
 };
-
+// save a user
 module.exports.saveAnUser = (req, res) => {
   const file = fs.readFileSync('user.json');
   const users = JSON.parse(file);
   const newUser = req.body;
-  users.push(newUser);
-
-  const allUsers = JSON.stringify(users);
-  fs.writeFile('user.json', allUsers, (err) => {
-    // error checking
-    if (err) throw err;
-    console.log('New user added');
-    res.send(allUsers);
-  });
+  if (Object.keys(newUser) == Object.keys(users[0])) {
+    users.push(newUser);
+    const allUsers = JSON.stringify(users);
+    fs.writeFile('user.json', allUsers, (err) => {
+      // error checking
+      if (err) throw err;
+      console.log('New user added');
+      res.send(allUsers);
+    });
+  } else {
+    res.send('Please ensure all the properties for saving a new user');
+  }
 };
 
 module.exports.updateUser = (req, res) => {
   const file = fs.readFileSync('user.json');
   const users = JSON.parse(file);
   const updateInfo = req.body;
-  const findUser = users.find((user) => user.id == updateInfo.id);
-  findUser.contact = updateInfo.contact;
-  const updatedUsers = JSON.stringify(users);
-  fs.writeFile('user.json', updatedUsers, (err) => {
-    // error checking
-    if (err) throw err;
-    console.log('New user added');
-    res.send('updated');
-  });
+  if (typeof updateInfo.id == 'number') {
+    const findUser = users.find((user) => user.id == updateInfo.id);
+    findUser.contact = updateInfo.contact;
+    const updatedUsers = JSON.stringify(users);
+    fs.writeFile('user.json', updatedUsers, (err) => {
+      // error checking
+      if (err) throw err;
+      console.log('New user added');
+      res.send('updated');
+    });
+  } else {
+    res.send('Please Provide an id of number !!!');
+  }
 };
 module.exports.updateMultipleUser = (req, res) => {
   const file = fs.readFileSync('user.json');
   const users = JSON.parse(file);
   const multipleUserInfo = req.body;
-  users.map((user) => {
-    multipleUserInfo.map((info) => {
-      if (user.id == info.id) {
-        user.contact = info.contact;
-      }
+  console.log(multipleUserInfo);
+  if (typeof multipleUserInfo.id == 'number') {
+    users.map((user) => {
+      multipleUserInfo.map((info) => {
+        if (user.id == info.id) {
+          user.contact = info.contact;
+        }
+      });
     });
-  });
-  const updatedUsers = JSON.stringify(users);
-  fs.writeFile('user.json', updatedUsers, (err) => {
-    // error checking
-    if (err) throw err;
-    console.log('New user added');
-    res.send('updated');
-  });
+    const updatedUsers = JSON.stringify(users);
+    fs.writeFile('user.json', updatedUsers, (err) => {
+      // error checking
+      if (err) throw err;
+      console.log('New user added');
+      res.send('updated');
+    });
+  } else {
+    res.send('Please provide a id of number ');
+  }
 };
 module.exports.deleteUser = (req, res) => {
   const file = fs.readFileSync('user.json');
   const users = JSON.parse(file);
   const deletedID = req.body.id;
-  const rest = users.filter((user) => user.id != deletedID);
-  const restUsers = JSON.stringify(rest);
-  fs.writeFile('user.json', restUsers, (err) => {
-    // error checking
-    if (err) throw err;
-    console.log('New user added');
-    res.send('User Deleted');
-  });
+  if (typeof deletedID == 'number') {
+    const rest = users.filter((user) => user.id != deletedID);
+    const restUsers = JSON.stringify(rest);
+    fs.writeFile('user.json', restUsers, (err) => {
+      // error checking
+      if (err) throw err;
+      console.log('New user added');
+      res.send('User Deleted');
+    });
+  } else {
+    res.send('please provide a id of number');
+  }
 };
